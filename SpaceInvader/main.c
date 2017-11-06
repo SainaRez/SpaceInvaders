1d39e2dfe2318d85c9ce1c23d008b51e233d97f9
Saina Rezvani <srezvani@wpi.edu>
2017-11-06 14:08:19 -0500

    Final code

--- SpaceInvader/main.c
+++ SpaceInvader/main.c
@@ -27,6 +27,7 @@ unsigned char currKey = 0;
 int level = 1;
 int randArray[6] = {};
 int location = 8;
+int locationArray[10] = {8,17,26,35,44,53,62,71,80,89};
 
 // Main
 void main(void){
@@ -100,9 +101,14 @@ void main(void){
            location = 8;
            makeArray();
            unsigned int k = 0;
+           /*
            while (k <= 3) {
            drawAliens();
-           k++;
+           k = k+1;
+           }*/
+           drawAliens();
+           while(k <= 50000) {
+               k++;
            }
            state++;
            break;
@@ -167,6 +173,115 @@ void main(void){
            break;
     }
 }// end while (1)
+
+   //This is test code
+
+    /*while (1)    // Forever loop
+        {
+           switch(state){
+           case 0:
+               Graphics_clearDisplay(&g_sContext); // Clear the display
+               Graphics_drawStringCentered(&g_sContext, "SPACE INVADERS", AUTO_STRING_LENGTH, 50, 50, TRANSPARENT_TEXT);
+               Graphics_flushBuffer(&g_sContext);
+               startGame(); //waits for '*' input to start game
+               state++;
+               break;
+           case 1:
+                  Graphics_clearDisplay(&g_sContext); // Clear the display
+                  Graphics_drawStringCentered(&g_sContext, "3!", AUTO_STRING_LENGTH, 48, 40, TRANSPARENT_TEXT);
+                  Graphics_flushBuffer(&g_sContext);
+                  swDelay(1);
+                  Graphics_clearDisplay(&g_sContext); // Clear the display
+                  Graphics_drawStringCentered(&g_sContext, "2!", AUTO_STRING_LENGTH, 48, 40, TRANSPARENT_TEXT);
+                  Graphics_flushBuffer(&g_sContext);
+                  swDelay(1);
+                  Graphics_clearDisplay(&g_sContext); // Clear the display
+                  Graphics_drawStringCentered(&g_sContext, "1!", AUTO_STRING_LENGTH, 48, 40, TRANSPARENT_TEXT);
+                  Graphics_flushBuffer(&g_sContext);
+                  swDelay(1);
+                  Graphics_clearDisplay(&g_sContext); // Clear the displays
+                  state++;
+              break;
+           case 2:          //Draw aliens
+               for (f = 0; f < 6; f++) {
+                    randArray[f] = 0;
+               }
+               unsigned int k = 0;
+               int l = 0;
+               for (l = 0; l < 10; l++) {
+               int location = locationArray[l];
+               makeArray();
+               while (k <= 3) {
+               drawAliens();
+               k++;
+               }
+             //Update screen, moving aliens down and looking for keypad input
+               moveAliens();
+               for (e = 0; e < 6; e++) {
+                   if (randArray[e] != 0) {
+                    isEmpty = 0;   //array is not all zero
+                    break;
+                   }
+                   else {
+                       isEmpty = 1; //array is all zero
+                   }
+               }
+               if (location == 89 && isEmpty == 0) {
+                   state++;
+               }
+               else if (location == 89 && isEmpty == 1) {
+                   if (level < 6) {
+
+                       Graphics_clearDisplay(&g_sContext);
+                       Graphics_drawStringCentered(&g_sContext, "Next Level!", AUTO_STRING_LENGTH, 48, 40, TRANSPARENT_TEXT);
+                       Graphics_flushBuffer(&g_sContext);
+                       swDelay(3);
+                       Graphics_clearDisplay(&g_sContext);
+                       level++;
+                       state = 2;
+                       break;
+                   }
+                   else {
+                       BuzzerOn();
+                       setLeds(15);
+                       Graphics_clearDisplay(&g_sContext);
+                       Graphics_drawStringCentered(&g_sContext, "YOU WON!", AUTO_STRING_LENGTH, 48, 40, TRANSPARENT_TEXT);
+                       Graphics_flushBuffer(&g_sContext);
+                       swDelay(3);
+                       Graphics_clearDisplay(&g_sContext);
+                       BuzzerOff();
+                       LedOff();
+                       level = 1;
+                       state = 0;
+                   }
+               }
+               }
+               break;
+           case 3:
+               Graphics_clearDisplay(&g_sContext); // Clear the display
+               k = 0;
+               while (k < 10) {
+                   setLeds(15);
+                   BuzzerOn();
+                   Graphics_drawStringCentered(&g_sContext, "You Suck", AUTO_STRING_LENGTH, 48, 40, TRANSPARENT_TEXT);
+                   Graphics_flushBuffer(&g_sContext);
+                   k++;
+               }
+               for (c = 0; c < 6; c++) {
+                   randArray[c] = 0;
+              }
+               location = 8;
+               BuzzerOff();
+               LedOff();
+               state = 0;
+               level = 1;
+               break;
+        }
+    }// end while (1) */
+
+
+
+
 } // end main
 
 void startGame(){
@@ -206,41 +321,53 @@ void drawAliens(){
         //randArray[i] = (rand() % 6) + 1;
         current = randArray[i];
         if(current == 1){
+            //Graphics_drawCircle(&g_sContext, 5, location, 6);
             Graphics_drawStringCentered(&g_sContext, "1", AUTO_STRING_LENGTH, 5, location, TRANSPARENT_TEXT);
-            Graphics_flushBuffer(&g_sContext);
+            //Graphics_flushBuffer(&g_sContext);
         }
         if(current == 2){
+            //Graphics_drawCircle(&g_sContext, 5, location, 6);
             Graphics_drawStringCentered(&g_sContext, "2", AUTO_STRING_LENGTH, 20, location, TRANSPARENT_TEXT);
-            Graphics_flushBuffer(&g_sContext);
+            //Graphics_flushBuffer(&g_sContext);
         }
         if(current == 3){
+            //Graphics_drawCircle(&g_sContext, 5, location, 6);
             Graphics_drawStringCentered(&g_sContext, "3", AUTO_STRING_LENGTH, 39, location, TRANSPARENT_TEXT);
-            Graphics_flushBuffer(&g_sContext);
+            //Graphics_flushBuffer(&g_sContext);
         }
         if(current == 4){
+            //Graphics_drawCircle(&g_sContext, 56, location, 6);
             Graphics_drawStringCentered(&g_sContext, "4", AUTO_STRING_LENGTH, 56, location, TRANSPARENT_TEXT);
-            Graphics_flushBuffer(&g_sContext);
+            //Graphics_flushBuffer(&g_sContext);
         }
         if(current == 5){
+            //Graphics_drawCircle(&g_sContext, 5, location, 6);
             Graphics_drawStringCentered(&g_sContext, "5", AUTO_STRING_LENGTH, 73, location, TRANSPARENT_TEXT);
-            Graphics_flushBuffer(&g_sContext);
+            //Graphics_flushBuffer(&g_sContext);
         }
         if(current == 6){
+            //Graphics_drawCircle(&g_sContext, 5, location, 6);
             Graphics_drawStringCentered(&g_sContext, "6", AUTO_STRING_LENGTH, 90, location, TRANSPARENT_TEXT);
-            Graphics_flushBuffer(&g_sContext);
+            //Graphics_flushBuffer(&g_sContext);
         }
+        Graphics_flushBuffer(&g_sContext);
     }
     return;
 }
 
 void moveAliens(){
-    int i = 0;
+    int row = 0;
     int newArray[6] = {};
-    while (i < 9) {
+    char key[1];
+    while (row < 9) {
         location = location + 9;
         Graphics_clearDisplay(&g_sContext);
+        //drawAliens();
+        //int rate = 11;
         unsigned int k = 0;
-        while (k <= 3) {
+        //while (k <= rate) {
+        int rate = 6  - level;
+        for (k = 0; k <= rate*10; k++) {
             drawAliens();
             char key[1];
             key[0] = getKey();
@@ -258,9 +385,31 @@ void moveAliens(){
                randArray[c] = newArray[c];
                newArray[c] = 0;
            }
-            k++;
+            //k = k+1;
+            //rate = rate - 2;
         }
-        i++;
+        /*drawAliens();
+        key[0] = getKey();
+        unsigned int keyPressed = atoi(key);
+        int x = 0;
+        int n;
+        for (n = 0; n < level; n++) {
+            if (randArray[n] != keyPressed) {
+                newArray[x] = randArray[n];
+                    x++;
+            }
+        }
+        int c;
+        for (c = 0; c < 6; c++) {
+            randArray[c] = newArray[c];
+            newArray[c] = 0;
+        }
+        int dRate = 50 * level;
+        while (k <= (1200 - dRate)) {
+            k = k+1;
+            key[0] = getKey();
+        }*/
+        row++;
     }
     return;
 }
